839067251274d1cd7b72c0852726485559aa7767
bz <boris.zaslavskiy@gmail.com>
2022-10-14 09:09:16 +0300

    1st commit

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import s from './Message.module.css'
+import avatar from "../avatar.png"
 
+export type MessageType = {
+    text: string
+    time: string
+}
+
+export type MessagesType = {
+    id: number
+    user: string
+    message: MessageType
+}
 // нужно создать правильный тип вместо any
-export type MessagePropsType = any
+export type MessagePropsType = {
+    message: MessagesType
+}
 
 // нужно отобразить приходящие данные
 const Message = (props: MessagePropsType) => {
@@ -12,6 +25,7 @@ const Message = (props: MessagePropsType) => {
                 <img
                     id={'hw1-avatar-' + props.message.id}
                     // создаёт студент
+                    src={avatar}
 
                     //
                 />
@@ -19,18 +33,21 @@ const Message = (props: MessagePropsType) => {
                     <div id={'hw1-name-' + props.message.id} className={s.name}>
                         {/*создаёт студент*/}
 
+                        {props.message.id}
+
+
                         {/**/}
                     </div>
                     <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
                         {/*создаёт студент*/}
-
+                        {props.message.message.text}
                         {/**/}
                     </pre>
                 </div>
             </div>
             <div id={'hw1-time-' + props.message.id} className={s.time}>
                 {/*создаёт студент*/}
-
+                {props.message.message.time}
                 {/**/}
             </div>
         </div>

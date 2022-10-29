1a0d14aef313741020f78efad5077836f19cb570
bz <boris.zaslavskiy@gmail.com>
2022-10-29 08:45:43 +0300

    3st commit

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -44,8 +44,8 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
 
         setName(e.currentTarget.value) // need to fix
-
-        error && setError('')
+        setError('')
+      //  error && setError('')
     }
     const addUser = () => {
     //    debugger;
